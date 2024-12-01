c78ea759322a52a66c1ac387a70b725a72eafb20
Ramkumar <110891811+Ram280797@users.noreply.github.com>
2024-12-01 19:50:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+// function redirectToPage() {
+  
+//     window.location.href = "https://www.netflix.com/in/";
+//   }
+
+  function redirectToPage() {
+   
+    const url = "https://www.netflix.com/in/";
+    window.open(url, '_blank'); 
+  }
